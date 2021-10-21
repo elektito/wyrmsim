c3cf6df53e9c0c2524432597fd3b5b753e44002b
Mostafa Razavi <mostafa@sepent.com>
2021-10-21 08:07:50 +0200

    Draw a smoother polygon

--- Wyrm.gd
+++ Wyrm.gd
@@ -4,18 +4,31 @@ func _ready():
 	update_poly()
 
 
+func _draw():
+	var outline_color := Color.black
+	var outline_width := 4.0
+	var poly = polygon
+	for i in range(1 , poly.size()):
+		draw_line(poly[i-1] , poly[i], outline_color, outline_width, true)
+	draw_line(poly[poly.size() - 1] , poly[0], outline_color, outline_width)
+
+
 func update_poly():
+	# construct a rectangle for each segment (each rectangle is an array of four points)
 	var rects := []
 	for child in get_children():
 		var width = child.get_node('rect').rect_size.x
 		var height = child.get_node('rect').rect_size.y
+		var growth = 1.8
 		var r = [
-			child.to_global(Vector2(-width / 2, -height / 2)),
-			child.to_global(Vector2(width / 2, -height / 2)),
-			child.to_global(Vector2(width / 2, height / 2)),
-			child.to_global(Vector2(-width / 2, height / 2)),
+			child.to_global(Vector2(-width / 2, -height / 2) * growth), # top-left
+			child.to_global(Vector2(width / 2, -height / 2) * growth),  # top-right
+			child.to_global(Vector2(width / 2, height / 2) * growth),   # bottom-right
+			child.to_global(Vector2(-width / 2, height / 2) * growth),  # bottom-left
 		]
 		rects.append(r)
+	
+	# now construct the polygon based on the rectangles
 	
 	var points := []
 	
@@ -23,15 +36,16 @@ func update_poly():
 	points.append(to_local(rects[0][2] + (rects[0][2] - rects[0][3])))
 	
 	for r in rects:
-		points.append(to_local(r[2]))
-		points.append(to_local(r[3]))
+		points.append(to_local((r[2] + r[3]) / 2))
 	
 	# the end of the tail
 	points.append(to_local(rects[-1][3] + 2*(rects[-1][3] - rects[-1][2])))
 	
 	for i in range(len(rects) - 1, -1, -1):
-		points.append(to_local(rects[i][0]))
-		points.append(to_local(rects[i][1]))
+		points.append(to_local((rects[i][0] + rects[i][1]) / 2))
+	
+	# top point of the head
+	points.append(to_local(rects[0][1] + Vector2.UP*10))
 	
 	polygon = PoolVector2Array(points)
 

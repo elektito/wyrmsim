d84daf64f18774bbe7bde07aed4fe604e7dc7eae
Mostafa Razavi <mostafa@sepent.com>
2021-10-30 00:50:10 +0200

    Add perpetual and deterministic building generation

--- World.gd
+++ World.gd
@@ -1,27 +1,22 @@
 extends Node2D
 
 const ROT_SPEED := 1.5
+const BLOCK_SIZE := 5000.0
+const BUILDING_MIN_WIDTH := 60.0
+const BUILDING_MAX_WIDTH := 300.0
 
 onready var camera := $Wyrm/Segment1/camera
 
+var noise = OpenSimplexNoise.new()
+var cur_blockx
+var block_buildings := {}
+
+
 func _ready():
 	randomize()
-	var x = 0
-	for i in range(1000):
-		var b = preload("res://Building.tscn").instance()
-		b.fill_rate = rand_range(0.04, 0.5)
-		b.outline_width = 3
-		b.outline_color = Color(0.2, 0.2, 0.2)
-		b.window_margin = 5
-		var windows = 4 + randi() % 10
-		var floors = 5 + randi() % 100
-		var width = windows * 10 + 3 * 2 + (windows + 1) * 2
-		var height  = floors * 10 + 3 * 2 + (floors + 1) * 2
-		var y = 1080 - height
-		b.rect_position = Vector2(x, y)
-		b.rect_size = Vector2(width, height)
-		x += width - b.outline_width
-		add_child(b)
+	noise.seed = randi()
+	update_buildings()
+
 
 func _input(event):
 	if Input.is_action_just_pressed("pause"):
@@ -74,3 +69,84 @@ func _physics_process(delta):
 			$Wyrm/Segment1.global_rotation = dir.angle()
 		else:
 			$Wyrm/Segment1.rotate(desired_rotation_speed * delta)
+
+
+func _on_building_generation_timer_timeout():
+	update_buildings()
+
+
+func update_buildings():
+	var x : float = to_local(camera.global_position).x
+	var blockx : float = x - fmod(x, BLOCK_SIZE)
+	if blockx == cur_blockx:
+		return
+	
+	var cur_blocks
+	var expected_blocks = [blockx - BLOCK_SIZE, blockx, blockx + BLOCK_SIZE]
+	
+	if cur_blockx == null:
+		cur_blocks = []
+	else:
+		cur_blocks = [cur_blockx - BLOCK_SIZE, cur_blockx, cur_blockx + BLOCK_SIZE]
+	
+	var removed_blocks = []
+	var new_blocks = []
+	for bx in cur_blocks:
+		if not bx in expected_blocks:
+			removed_blocks.append(bx) 
+	for bx in expected_blocks:
+		if not bx in cur_blocks:
+			new_blocks.append(bx)
+	for bx in removed_blocks:
+		remove_block(bx)
+	for bx in new_blocks:
+		fill_block(bx)
+	
+	cur_blockx = blockx
+
+
+func fill_block(bx: float):
+	var x := bx
+	var remaining_width := BLOCK_SIZE
+	var rng := RandomNumberGenerator.new()
+	rng.seed = noise.get_noise_1d(bx)
+	while x < bx + BLOCK_SIZE:
+		var max_width = min(BUILDING_MAX_WIDTH, remaining_width)
+		var width = rng.randf_range(BUILDING_MIN_WIDTH, max_width)
+		remaining_width -= width
+		if remaining_width > 0 and remaining_width < BUILDING_MIN_WIDTH:
+			# we might go a bit above max width, but we won't have next building too narrow (and thus unrenderable)
+			width += remaining_width
+		var building = generate_building(x, width)
+		add_child(building)
+		x += width
+		if bx in block_buildings:
+			block_buildings[bx].append(building)
+		else:
+			block_buildings[bx] = [building]
+
+
+func remove_block(bx):
+	for b in block_buildings[bx]:
+		b.queue_free()
+	block_buildings.erase(bx)
+
+
+func generate_building(x: float, width: float):
+	var rng := RandomNumberGenerator.new()
+	rng.seed = hash(noise.get_noise_1d(x))# * float(1 << 63)
+	var seed_value := rng.randi()
+	var b = preload("res://Building.tscn").instance()
+	b.rng_seed = seed_value
+	b.fill_rate = rng.randf_range(0.04, 0.5)
+	b.outline_width = 3
+	b.outline_color = Color(0.2, 0.2, 0.2)
+	b.window_margin = 5
+	var floors = 5 + rng.randi() % 100
+	var height  = floors * 10 + 3 * 2 + (floors + 1) * 2
+	var y = 1080 - height
+	b.rect_position = Vector2(x, y)
+	b.rect_size = Vector2(width, height)
+	
+	return b
+

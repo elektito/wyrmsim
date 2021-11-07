26178bc6cb0a3be14c4896effa3cbc51333cb188
Mostafa Razavi <mostafa@sepent.com>
2021-11-07 01:08:52 +0100

    Update collectible sprite
     - Varying start phase
     - Changing colors

--- Collectible.gd
+++ Collectible.gd
@@ -5,6 +5,7 @@ signal collected()
 var rotation_dir := 1.0
 var rotation_speed := PI / 4
 var building_x
+onready var start_phase := randf() * 1000.0
 
 func _ready():
 	rotation_dir = 1.0 if randf() < 0.5 else -1.0
@@ -12,8 +13,10 @@ func _ready():
 
 func _physics_process(delta):
 	rotation += rotation_dir * rotation_speed * delta
-	var phase = abs(sin(OS.get_ticks_msec() * 0.001)) * 0.8 + 0.2
+	var phase = abs(sin(OS.get_ticks_msec() * 0.001 + start_phase)) * 0.8 + 0.2
 	scale = Vector2(phase, phase)
+	modulate = Color.gray * phase
+	
 
 
 func _on_Collectible_area_entered(area):
